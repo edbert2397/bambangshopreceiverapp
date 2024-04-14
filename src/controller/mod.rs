3528a72a45852bf2d087924acd458e47dfe800c3
edbert2397 <edberthalim29@gmail.com>
2024-04-14 18:14:01 +0700

    Create Notification service struct skeleton.

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,5 +1,7 @@
 use rocket::fairing::AdHoc;
 
+pub mod notificiation;
+
 pub fn route_stage() -> AdHoc {
     return AdHoc::on_ignite("Initializing controller routes...", |rocket| async {
         rocket
