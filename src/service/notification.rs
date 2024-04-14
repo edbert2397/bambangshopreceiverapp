d522e28ac64dcd35232c095f16f5f71130a16436
edbert2397 <edberthalim29@gmail.com>
2024-04-14 20:17:40 +0700

    Implement receive_notification function in Notification service. and Implement the receive function in Notification controller.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -1,7 +1,6 @@
 use std::fmt::format;
 use std::thread;
 
-use rocket::http::hyper::request;
 use rocket::http::Status;
 use rocket::log;
 use rocket::serde::json::to_string;
@@ -9,7 +8,7 @@ use rocket::tokio;
 
 use bambangshop_receiver::{APP_CONFIG, REQWEST_CLIENT, Result, compose_error_response};
 use crate::model::notification::{self, Notification};
-use crate::model::subscriber::SubscriberRequest;
+use crate::model::subscriber::{self, SubscriberRequest};
 use crate::repository::notification::NotificationRepository;
 
 pub struct NotificationService;
@@ -74,5 +73,10 @@ impl NotificationService{
     pub fn unsubscribe(product_type: &str)->Result<SubscriberRequest>{
         let product_type_clone = String::from(product_type);
         return thread::spawn(move|| Self::unsubscribe_request(product_type_clone)).join().unwrap();
+    }
+
+    pub fn receive_notification(payload: Notification)->Result<Notification>{
+        let subscriber_result: Notification = NotificationRepository::add(payload);
+        return Ok(subscriber_result);
     }
 }
